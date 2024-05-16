fc35f327906bfae1f89eac1c78a121ffd6293db0
JamboPE <github@jdpe.co.uk>
2024-05-16 03:45:16 +0100

    Added debug print statements

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pythonosc import udp_client
 
 atem_ip = "144.32.210.2"
 X32_ip =  "144.32.210.3"
-leftMics = [0,1] # zero infexed lol
-rightMics = [2,3] # zero infexed lol
-threshold = 0
+leftMics = [0,1] # zero indexed lol
+rightMics = [2,3] # zero indexed lol
+micLevelThreshold = 0
 
  # Create ATEMMax object
 switcher = PyATEMMax.ATEMMax()
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     ATEM.connect(atem_ip)
     ATEM.preview(1)
     ATEM.program(1)
+
     while True:
         while send_osc_command(X32_ip, 10023, "/config/mute/1") == "OFF" and switcher.programInput[0].videoSource.value != 7: # Only runs autovis if a VT isn't playing (detected based on X32 mute group being on) and the TV mix isn't slected
             meters = []
@@ -40,18 +41,25 @@ if __name__ == "__main__":
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 1")) # get levels of mic 2
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 2")) # get levels of mic 3
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 3")) # get levels of mic 4
-            if max(meters) > threshold:
+
+            if max(meters) > micLevelThreshold:
                 active_mic = meters.index(max(meters)) # get index of mic with highest level (above threshold)
                 if active_mic in leftMics and preview_id != 1:
+                    print("Hearing noise in mic", int(active_mic)+1, "previewing camera 1")
                     ATEM.preview(1)
                 elif active_mic in leftMics and preview_id == 1:
+                    print("Still hearing noise in mic", int(active_mic)+1, "cutting to camera 1")
                     ATEM.program(1)
                 if active_mic in rightMics and preview_id != 3:
+                    print("Hearing noise in mic", int(active_mic)+1, "previewing camera 3")
                     ATEM.preview(3)
                 elif active_mic in rightMics and preview_id == 3:
+                    print("Still hearing noise in mic", int(active_mic)+1, "cutting to camera 1")
                     ATEM.program(3)
             else:
                 if preview_id != 2:
+                    print("No significant noise detected in any mics, previewing camera 2")
                     ATEM.preview(2)
                 if preview_id == 2:
+                    print("Still no significant noise detected in any mics, cutting to camera 2")
                     ATEM.program(2)

cc4719b28af8bcf1c9b0c5cc4a8877509548a986
JamboPE <github@jdpe.co.uk>
2024-05-16 03:50:25 +0100

    Change TV mix intergration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PyATEMMax
 from pythonosc import udp_client
+import time
 
 atem_ip = "144.32.210.2"
 X32_ip =  "144.32.210.3"
@@ -35,14 +36,19 @@ if __name__ == "__main__":
     ATEM.program(1)
 
     while True:
-        while send_osc_command(X32_ip, 10023, "/config/mute/1") == "OFF" and switcher.programInput[0].videoSource.value != 7: # Only runs autovis if a VT isn't playing (detected based on X32 mute group being on) and the TV mix isn't slected
+        while send_osc_command(X32_ip, 10023, "/config/mute/1") == "OFF": # Only runs autovis if a VT isn't playing (detected based on X32 mute group being on)
+            print("Getting mic levels...")
             meters = []
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 0")) # get levels of mic 1
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 1")) # get levels of mic 2
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 2")) # get levels of mic 3
             meters.append(send_osc_command(X32_ip, 10023, "/meters/6 3")) # get levels of mic 4
+            print("Mic levels:", meters)
 
-            if max(meters) > micLevelThreshold:
+            if switcher.programInput[0].videoSource.value == 7: # If TV mix is selected, wait 4 seconds
+                    print("TV mix selected, waiting 4 seconds")
+                    time.sleep(4)
+            elif max(meters) > micLevelThreshold:
                 active_mic = meters.index(max(meters)) # get index of mic with highest level (above threshold)
                 if active_mic in leftMics and preview_id != 1:
                     print("Hearing noise in mic", int(active_mic)+1, "previewing camera 1")
